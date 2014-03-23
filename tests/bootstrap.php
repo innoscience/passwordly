6d818a5549ba46a0402be1ab0c37a7a21e79a7a1
Brandon Fenning <brandon@innoscience.net>
2014-03-23 11:41:53 -0400

    Require once

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,10 +3,10 @@
 error_reporting(E_ALL | E_STRICT);
 
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-	require __DIR__ . '/../vendor/autoload.php';
+	require_once __DIR__ . '/../vendor/autoload.php';
 }
-elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
-	require __DIR__ . '/../../../autoload.php';
+else if (file_exists(__DIR__ . '/../../../autoload.php')) {
+	require_once __DIR__ . '/../../../autoload.php';
 }
 else {
 	throw new \Exception('Can\'t find autoload.php. Did you install dependencies via composer?');
